d2cb54c1e2a79aa5c0e714a120bd9725740b1c81
alexgreen932 <alexgreen932@gmail.com>
2024-01-06 21:28:40 +0300

    bf change css

--- hooks.php
+++ hooks.php
@@ -29,10 +29,6 @@ if (!defined('ABSPATH'))
  */
 add_action( 'gform_entry_detail', 'add_to_details', 10, 2 );
 function add_to_details( $form, $entry ) {
-    // d($entry);//dev
-    // if ( $form['id'] == 1 ) {
-        // echo '<div>This hook is used to add additional information to the details page for an entry.</div>';
-    // }
     $data = json_decode($entry[1]);
     echo '<div class="j-admin-block">';
     echo '<h2>Solar Form Data</h2>';
@@ -43,21 +39,7 @@ function add_to_details( $form, $entry ) {
         echo '</div>';
     }
     echo '</div>';
-    // $html = '';
-    // $html .= '<div class="j-inline-field">';
-    // $html .= '<div class="j-inline-field"><label>Any Label:</label>Here can be any field which is important ex Name, Email</div>';
-    // $html .= '<div class="j-inline-field"><label>Dachfläche:</label>' . $data->Dachfläche .'</div>';
-    // $html .= '<div class="j-inline-field">ETC</div>';
-    // $html .= '<div class="j-inline-field">ETC</div>';
-    // // $html .= get_current_screen()->base;
-    // echo $html;
 }
-
-
-// add_filter('gform_entries_column_values', 'change_column_data', 10, 4);
-// function change_column_data($value, $form, $field, $entry) {
-//     return '9999';
-// }
 
 /**
  * replaces json with data via foreach
